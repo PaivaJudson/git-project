8d8439e406b9635521d26eada4f8109f42641403
Judson Paiva <judson.paiva@hotmail.com>
2019-01-20 23:17:58 +0100

    efectuando o segundo commit

---
+++ ConexaoMYSQL.java
@@ -0,0 +1,21 @@
+package pacotes.codigo.fonte.conexaoBanco;
+
+import java.sql.Connection;
+
+public class ConexaoMYSQL {
+    
+    public static String status = "não conectado!";
+
+    public ConexaoMYSQL() {
+        
+    }
+    
+    public static Connection getConexaoMySQL(){
+        Connection connection = null;
+        
+        return connection;
+    }
+    
+    
+    
+}
